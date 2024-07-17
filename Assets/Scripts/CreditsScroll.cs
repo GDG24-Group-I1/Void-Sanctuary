4bfb13b810db6f1c5bef54b9cbd517d55b0ebee6
Alessio Rosiello <alex.robassomero@gmail.com>
2024-07-17 11:48:39 +0200

    Add credits (text to be written)

---
+++ Assets/Scripts/CreditsScroll.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsScroll : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 1.0f;
+    [SerializeField] private float stopPoint = 200;
+    private Timer timer;
+    private bool started;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = new Timer()
+        {
+            OnTimerElapsed = () =>
+            {
+                started = true;
+                return null;
+            }
+        };
+        timer.Start(2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started) return;
+        transform.position = transform.position.ShiftBy(y: Time.deltaTime * scrollSpeed);
+        if (transform.position.y > stopPoint)
+        {
+            started = false;
+        }
+    }
+
+    public void OnMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
